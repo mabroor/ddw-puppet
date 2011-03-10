defb1de697ae73d3b506e4d8f981ab824f8936c2
Eivind Uggedal <eivind@uggedal.com>
2011-03-10 16:26:06 +0000

    Cloning Mingus src, update Pip version, and install Mingus requirements.

--- manifests/templates.pp
+++ manifests/templates.pp
@@ -28,7 +28,26 @@ class webserver {
 
   include virtualenv
 
-  virtualenv::environment { "$root/myblog": }
+  $blog_venv = "$root/myblog"
+  virtualenv::environment { $blog_venv: }
+
+  $mingus_root = "$root/myblog/django-mingus"
+  exec { "mingus-src":
+    command => "git clone git://github.com/montylounge/django-mingus.git $mingus_root",
+    creates => $mingus_root,
+    require => [File[$root], Virtualenv::Environment[$blog_venv]],
+  }
+
+  exec { "update-pip":
+    command => "$blog_venv/bin/pip install -U pip",
+    onlyif => "test `$blog_venv/bin/pip --version | cut -f 2 -d ' '` = '0.8.1'",
+  }
+
+  exec { "mingus-requirements":
+    command => "$blog_venv/bin/pip install -r $mingus_root/mingus/stable-requirements.txt && touch $blog_venv/MINGUS_REQUIREMENTS_INSTALLED",
+    unless => "/bin/sh -c '[ -f $blog_venv/MINGUS_REQUIREMENTS_INSTALLED ]'",
+    require => [Exec["mingus-src"], Exec["update-pip"]],
+  }
 }
 
 class dbserver {
