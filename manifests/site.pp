4e977a172b250999f156d196bef20ad08243ad4b
Eivind Uggedal <eivind@uggedal.com>
2011-03-10 15:10:08 +0000

    Default file owner and permissions and path.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,12 @@
 import "templates"
 import "nodes"
+
+File {
+  owner => root,
+  group => root,
+  mode => 644,
+}
+
+Exec {
+  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
