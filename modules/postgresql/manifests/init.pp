48a6185636489dff350743a9a00268a93d4b92e8
Eivind Uggedal <eivind@uggedal.com>
2011-03-11 13:12:24 +0100

    Ability to create and delete postgresql databases.

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -29,3 +29,39 @@ define postgresql($listen_address="127.0.0.1", $allowed_ips=[]) {
     subscribe => [Package["postgresql"], File["$pgsql_root/postgresql.conf"], File["$pgsql_root/pg_hba.conf"]],
   }
 }
+
+define postgresql::database($owner, $ensure=present) {
+  $userexists = "psql --tuples-only -c 'SELECT rolname FROM pg_catalog.pg_roles;' | grep '^ ${owner}$'"
+  $dbexists = "psql -ltA | grep '^$name|'"
+
+  if $ensure == 'present' {
+
+    exec { "createdb $name":
+      command => "createdb -O $owner $name",
+      user => "postgres",
+      unless => $dbexists,
+      require => Exec["createuser $owner"],
+    }
+
+    exec { "createuser $owner":
+      command => "createuser --no-superuser --no-createdb --no-createrole ${owner}",
+      user => "postgres",
+      unless => $userexists,
+    }
+
+  } elsif $ensure == 'absent' {
+
+    exec { "dropdb $name":
+      command => "dropdb $name",
+      user => "postgres",
+      onlyif => $dbexists,
+    }
+
+    exec { "dropuser $owner":
+      command => "dropuser ${owner}",
+      user => "postgres",
+      onlyif => $userexists,
+      require => Exec["dropdb $name"],
+    }
+  }
+}
