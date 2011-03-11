80b7619d112cbcf52219764589eca67968dc5763
Eivind Uggedal <eivind@uggedal.com>
2011-03-11 12:07:58 +0000

    Database settings are node specific and does not belong in a server template. Same for mingus.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,12 +4,19 @@ node basenode {
 
 node "web1.uggedal.com" inherits basenode {
   include webserver
+  include mingus
 }
 
 node "web2.uggedal.com" inherits basenode {
   include webserver
+  include mingus
 }
 
 node "db1.uggedal.com" inherits basenode {
   include dbserver
+
+  postgresql { "standard":
+    listen_address => "74.207.233.129",
+    allowed_ips => ["74.207.233.12", "74.207.233.124"],
+  }
 }
--- manifests/templates.pp
+++ manifests/templates.pp
@@ -26,12 +26,7 @@ class webserver {
     ensure => present,
   }
 
-  include mingus
 }
 
 class dbserver {
-  postgresql { "standard":
-    listen_address => "74.207.233.129",
-    allowed_ips => ["74.207.233.12", "74.207.233.124"],
-  }
 }
